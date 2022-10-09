43c91f70322f2ba683eb7dc38cf4b9d5052797ab
Akarina_GX <ironpool99@gmail.com>
2022-10-09 15:36:52 +0500

    13123132

--- index.php
+++ index.php
@@ -1,1 +1,25 @@
-121212122
+<?
+
+
+
+$ch = curl_init('https://prog-time.ru/');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_HEADER, false);
+$html = curl_exec($ch);
+curl_close($ch);
+echo $html;
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
